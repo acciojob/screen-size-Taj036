b056d247da35b2ccece624f0e5c0fa72ba3cee7f
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-06 14:15:42 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateSize(){
+	let width =  window.innerWidth;
+	let height =  window.innerHeight;
+
+	let text = document.querySelector('h1') ;
+	text.textContent = `Width: ${width} and Height: ${height}`;
+	
+}
+window.addEventListener('resize' , updateSize )
+updateSize()
